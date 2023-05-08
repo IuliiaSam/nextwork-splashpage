cd173390251070029396af24127b094f19f662be
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-05-07 17:42:33 -0700

    Saved pulled files from main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import NavBar from '../components/navbar'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import NavBar from "../components/navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      </main>
+      <NavBar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
     </>
-  )
+  );
 }
